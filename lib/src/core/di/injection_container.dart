840293fc1984b1deaa6ee54c174db3a3a909a17f
Jason khongwir <50296658+jsonOdison@users.noreply.github.com>
2025-10-29 13:00:11 +0530

    added the dependency injection for it

--- lib/src/core/di/injection_container.dart
+++ lib/src/core/di/injection_container.dart
@@ -1,10 +1,24 @@
 import 'package:get_it/get_it.dart';
 import 'package:hive/hive.dart';
 import 'package:kuvaka_tech_assesment/src/core/constants/hive_keys.dart';
+import 'package:kuvaka_tech_assesment/src/features/transactions/data/local/transaction_local_data_source.dart';
+import 'package:kuvaka_tech_assesment/src/features/transactions/data/models/transaction_model.dart';
+import 'package:kuvaka_tech_assesment/src/features/transactions/domain/repositories/transaction_repository.dart';
+import 'package:kuvaka_tech_assesment/src/features/transactions/domain/repositories/transaction_repository_impl.dart';
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
   // hive box instance
-  final transactionBox = Hive.box(HiveKeys.transactions);
+  final transactionBox = Hive.box<TransactionModel>(HiveKeys.transactions);
+
+  // Data source
+  sl.registerLazySingleton<TransactionLocalDataSource>(
+    () => TransactionLocalDataSourceImpl(transactionBox),
+  );
+
+  // Repository
+  sl.registerLazySingleton<TransactionRepository>(
+    () => TransactionRepositoryImpl(localDataSource: sl()),
+  );
 }
